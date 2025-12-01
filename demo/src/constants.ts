24e7e715a269902a87ef8a42d4de0794f6f3e0ff
Andrew Meyer <andrew@rewdy.com>
2025-12-01 09:15:41 -0600

    chore: add check and format jobs

--- demo/src/constants.ts
+++ demo/src/constants.ts
@@ -1,6 +1,6 @@
 export const SITE = {
-	title: "Aeraline",
-	url: "https://aeraline.rewdy.lol",
-	description:
-		"Aeraline is an ultralight CSS library, featuring ratio-based styles for semantic HTML elements.",
+  title: "Aeraline",
+  url: "https://aeraline.rewdy.lol",
+  description:
+    "Aeraline is an ultralight CSS library, featuring ratio-based styles for semantic HTML elements.",
 };
